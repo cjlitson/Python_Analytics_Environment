352275f1cdd1e295146483f7a12936e3946a268a
cjlitson <43155876+cjlitson@users.noreply.github.com>
2025-08-29 14:46:50 -0700

    Remove unused AzCopy example script

--- setup/Run-Setup.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "%~dp0setup\Install-Guided.ps1"
-pause
--- setup/Run-User.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "%~dp0setup\Install-User.ps1"
-pause
